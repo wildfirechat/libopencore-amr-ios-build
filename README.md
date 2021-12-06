2800ffcce4e3001836352f832ab83e90053479a4
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2021-12-06 13:56:36 +0800

    add source code and script

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # libopencore-amr-ios-build
 libopencore-amr library source files and iOS build script
+
+## opencore-amr source code
+From https://sourceforge.net/projects/opencore-amr/files/opencore-amr/opencore-amr-0.1.5.tar.gz
+
+## How to build
+```
+git clone https://github.com/wildfirechat/libopencore-amr-ios-build.git
+cd libopencore-amr-ios-build
+./build_libopencore_arm.sh
+```
---
+++ build_libopencore_amr.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+set -e
+stty -echo
+
+VERSION="0.1.5"
+
+CURRENTPATH=`pwd`
+rm -rf "${CURRENTPATH}/src"
+mkdir -p "${CURRENTPATH}/src"
+tar zxf opencore-amr-${VERSION}.tar.gz -C "${CURRENTPATH}/src"
+cd "${CURRENTPATH}/src/opencore-amr-${VERSION}"
+
+DEVELOPER=`xcode-select -print-path`
+DEST="${CURRENTPATH}/opencore-amr-iphone"
+rm -rf "${DEST}"
+mkdir -p "${DEST}"
+
+TARGETS="arm64 x86_64 arm64_sim"
+LIBS="libopencore-amrnb.a libopencore-amrwb.a"
+
+./configure
+for target in $TARGETS; do
+    make clean
+    case $target in
+    arm64)
+        arch=arm64
+        IOSMV=" -miphoneos-version-min=7.0"
+        echo ""
+        echo ""
+        echo ""
+        echo "******** Building opencore-amr for iPhoneOS $arch ********"
+        PATH=`xcodebuild -version -sdk iphoneos PlatformPath`"/Developer/usr/bin:$PATH" \
+        SDK=`xcodebuild -version -sdk iphoneos Path` \
+        CXX="xcrun --sdk iphoneos clang++ -arch $arch $IOSMV --sysroot=$SDK -isystem $SDK/usr/include -fembed-bitcode" \
+        LDFLAGS="-Wl" \
+        ./configure \
+        --host=arm-apple-darwin \
+        --prefix=$DEST \
+        --disable-shared
+        ;;
+    arm64_sim)
+        arch=arm64
+        IOSMV=" -mios-simulator-version-min=7.0"
+        echo ""
+        echo ""
+        echo ""
+        echo "******** Building opencore-amr for iPhoneSimulator $arch ********"
+        PATH=`xcodebuild -version -sdk iphonesimulator PlatformPath`"/Developer/usr/bin:$PATH" \
+        SDK=`xcodebuild -version -sdk iphonesimulator Path` \
+        CXX="xcrun --sdk iphonesimulator clang++ -arch $arch $IOSMV --sysroot=$SDK -isystem $SDK/usr/include -fembed-bitcode" \
+        LDFLAGS="-Wl" \
+        ./configure \
+        --host=arm-apple-darwin \
+        --prefix=$DEST \
+        --disable-shared
+        ;;
+    *)
+        arch=x86_64
+        IOSMV=" -mios-simulator-version-min=7.0"
+        echo ""
+        echo ""
+        echo ""
+        echo "******** Building opencore-amr for iPhoneSimulator $arch ********"
+        PATH=`xcodebuild -version -sdk iphonesimulator PlatformPath`"/Developer/usr/bin:$PATH" \
+        CXX="xcrun --sdk iphonesimulator clang++ -arch $arch $IOSMV -fembed-bitcode" \
+        ./configure \
+        --host=$arch \
+        --prefix=$DEST \
+        --disable-shared
+        ;;
+    esac
+    make -j10
+    make install
+    mkdir -p $DEST/lib/$target/
+    for i in $LIBS; do
+        mv $DEST/lib/$i $DEST/lib/$target/
+    done
+done
+
+echo ""
+echo ""
+echo ""
+echo "******** Create xcframework.********"
+
+mkdir -p $DEST/lib/universal_sim
+mkdir -p $DEST/output
+
+for i in $LIBS; do
+lipo -create -output $DEST/lib/universal_sim/$i $DEST/lib/x86_64/$i $DEST/lib/arm64_sim/$i
+input="-library $DEST/lib/arm64/$i -headers $DEST/include -library $DEST/lib/universal_sim/$i -headers $DEST/include"
+xcodebuild -create-xcframework $input -output $DEST/output/$i.xcframework
+open $DEST/output
+done
